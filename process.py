c2a534eeb9c6f60417b2beb82163f20e13ef2322
Zak Liles <zak.liles@gmail.com>
2022-01-23 22:27:03 -0700

    Completed Parts 1, 3 & Extra Credit

--- process.py
+++ process.py
@@ -1,3 +1,6 @@
+from cmath import log
+
+
 log_file = open("um-server-01.txt") #opens file for reading in text mode
 
 #creates sales_reports() function taking in one file input
@@ -7,6 +10,18 @@ def sales_reports(log_file):
         day = line[0:3] # day is equal to first three characters of the line (Mon, Tue, Wed etc.)
         if day == "Mon": # check if day is tuesday
             print(line) # prints all lines from tuesdays
+    log_file.seek(0)
 
 
 sales_reports(log_file) # calls sales_reports function on log file
+
+def ten_melons(log_file):
+    for line in log_file:
+        line = line.rstrip()
+        line_arr = line.split(' ')
+        quantity = int(line_arr[2])
+        items = line_arr[3:-1]
+        if any('melon' in ele for ele in items) and quantity >= 10:
+            print(line)
+
+ten_melons(log_file)
